ac8b0676974a65b672cf399599ed3021d29a9d8d
Sakis Ahmed <sakis.ahmed.insat@gmail.com>
2021-11-04 17:23:11 +0100

    first commit

---
+++ src/components/Address.js
@@ -0,0 +1,14 @@
+import React from "react";
+export default function FullName() {
+    const user = 
+        {
+          id: 1,
+          name: 'Sakis Ahmed',
+          adress: '32 Rue Tah houcine Ezzahra',
+        }
+    return (
+        <div className="">
+          {user.adress}
+        </div>
+      );
+}
---
+++ src/components/ProfilPhoto.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ProfilPhoto from './Photo.png';
+
+export default function ProfilP() {
+    return (
+        <div className="App">   
+            <img src={ProfilPhoto} alt="photo De Profil"/>          
+    </div>
+      );
+}
